fb08236ce3a86108419a98e90765ea5162e055be
Yaroslav Selyutin <moontesearch@gmail.com>
2026-03-23 23:44:06 +1000

    feat: реализует пример good repository

--- backend/internal/menu/products/entity/product/product-repository.go
+++ backend/internal/menu/products/entity/product/product-repository.go
@@ -1,21 +1,24 @@
 package product
 
 import (
+	productAllergen "github.com/scrumno/scrumno-api/internal/menu/products/entity/product-allergen"
+	customerTag "github.com/scrumno/scrumno-api/internal/menu/products/entity/product-customer-tag"
+	productSize "github.com/scrumno/scrumno-api/internal/menu/products/entity/product-size"
 	factory "github.com/scrumno/scrumno-api/shared/factories/gorm"
 	"github.com/scrumno/scrumno-api/shared/interfaces/base"
 	"gorm.io/gorm"
 )
 
-type ProductRepository interface {
-	base.BaseRepository[Product]
+type ProductRepository struct {
+	SizeRepository        base.BaseRepository[productSize.ProductSize]
+	AllergenRepository    base.BaseRepository[productAllergen.ProductAllergen]
+	CustomerTagRepository base.BaseRepository[customerTag.ProductCustomerTag]
 }
 
-type productRepository struct {
-	base.BaseRepository[Product]
-}
-
-func NewProductRepository(db *gorm.DB) ProductRepository {
-	return &productRepository{
-		BaseRepository: factory.NewGormRepository[Product](db),
+func NewProductRepository(db *gorm.DB) *ProductRepository {
+	return &ProductRepository{
+		SizeRepository:        factory.NewGormRepository[productSize.ProductSize](db),
+		AllergenRepository:    factory.NewGormRepository[productAllergen.ProductAllergen](db),
+		CustomerTagRepository: factory.NewGormRepository[customerTag.ProductCustomerTag](db),
 	}
 }
